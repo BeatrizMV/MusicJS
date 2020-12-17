86f0580414142df87986f86588ada4bcdd49797d
Isaac Rovira <iroviraro@uoc.edu>
2020-12-18 00:57:39 +0100

    Dos funciones leer de Firebase Además están suscritas a los cambios en los valores de los documentos de Firebase.

--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -4,6 +4,8 @@ import {SONGS} from '../../assets/data/mock-songs'
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import { SIGBREAK } from 'constants';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 
 @Injectable({
@@ -11,20 +13,53 @@ import {map} from 'rxjs/operators';
 })
 export class SongService {
   getSongs(): Song[] {
-    this.getAlbums();
+    //this.getSongsFB();
+    this.getSongsFB2().subscribe(console.log);
     return SONGS
   }
 
-  getAlbums(){
 
-    var album = this.firestore.collection('album_alt').get();
-    album.forEach(doc=>{
-      doc.forEach(d=>{
-        console.log(d.data())
-        console.log(d.id)
-        console.log(d.metadata)
-      });
+  /*
+  album: number;
+  track: number;
+  title: string;
+  composer: string[];
+  performer: string [];
+  time: string;
+  year?:string;
+  localPath: string;
+   */
+
+   //Este método devuelve todos los documentos de la colección "song" y se suscribe de manera q recibe los cambios
+   //q se producen en cualquier documento de la base de datos.
+   getSongsFB(){
+    var songs: Song[];
+    this.firestore.collection('songs').snapshotChanges().subscribe(song =>{
+      song.map(song =>{
+        console.log(song.payload.doc.data())
+      })
     })
+  }
+
+  /*
+  Este método devuelve un array "song" con los datos de la colección "song" y tb se suscribe.
+  He cambiado atributo album en los documentos para que sea un "reference" a los documentos de la colección albums.
+  El valor que devuelve no es una cadena pero se le peude pasar directametne como referencia a firestore.
+  */
+  getSongsFB2(){
+    return this.firestore.collection('songs').snapshotChanges().pipe(map(snaps =>{
+      return snaps.map(snap =>{
+        return <Song>{
+          album: snap.payload.doc.get('album'),
+          track: snap.payload.doc.get('track'),
+          composer: snap.payload.doc.get('composer'),
+          performer: snap.payload.doc.get('performer'),
+          time: snap.payload.doc.get('time'),
+          year: snap.payload.doc.get('year'),
+          localPath: snap.payload.doc.get('localPath'),
+        }
+      })
+    }))
   }
 
 
